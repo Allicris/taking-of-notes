b75f13439e29ea80fe585fa1c5995710d7dc9b92
Allison Serrano <allisonserrano171@yahoo.com>
2023-07-19 20:24:54 -0400

    added more comments and debugged more codes.

--- server.js
+++ server.js
@@ -13,25 +13,28 @@ const PORT = process.env.PORT || 3001;
 //Allows us to use express middleware
 const app = express();
 
-//Allows us to use this file as a data base to store content
+//Allows us to use this file as a data base to store and add content
 const dataBase = require('./Develop/db/db.json')
 
 //Configuring express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+//Made sure that I was targeting the right folder. I kept recieving an error message when I only typed "public"
 app.use(express.static("./Develop/public"));
 
-//Generates Unique ID's 
+//Generates Unique ID's for notes
 const uniqueId = require('generate-unique-id');
 
+//Targets the index.html folder
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './Develop/public/index.html'));
   });  
 
+  //Targets the notes folder
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './Develop/public/notes.html'))
 });
-
+//Targets the notes db.json and sends it back to the client
 app.get('/api/notes', (req, res) => {
     try {
         const data = fs.readFileSync('./Develop/db/db.json', 'utf8');
@@ -42,7 +45,7 @@ app.get('/api/notes', (req, res) => {
         res.json(err);
       }
 });
-
+//Allows us add new notes by pushing it to the db.json file
 app.post('/api/notes', (req, res) => {
     const newNote = {
         title: req.body.title,
@@ -51,7 +54,7 @@ app.post('/api/notes', (req, res) => {
     }
     dataBase.push(newNote);
     fs.writeFileSync(path.join(__dirname, './Develop/db/db.json'), JSON.stringify(dataBase, null, 2));
-    const newData = JSON.parse(fs.readFileSync('./Develop/db/db.json', 'utf-8'))
+    newData = JSON.parse(fs.readFileSync('./Develop/db/db.json', 'utf-8'))
   res.json(newData);
 })
 
